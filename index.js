d69ffb83b979b195524d802610c3e71c1dbe1a42
KUTIKUPPALA SRI HARSHA <sriharshakutikuppala2002@gmail.com>
2024-06-06 21:25:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    document.body.style.backgroundColor = "#e0e0e0";
+    document.getElementById("hi").textContent="#e0e0e0";
+}
+function myFunction1() {
+    document.body.style.backgroundColor = "#6fcf97";
+    document.getElementById("hi").textContent="#6fcf97";
+} 
+function myFunction2(){
+    document.body.style.backgroundColor="#56ccf2";
+    document.getElementById("hi").textContent="#56ccf2";
+}
+function myFunction3(){
+    document.body.style.backgroundColor="#ff69b4"
+    document.getElementById("hi").textContent="#ff69b4";
+}
+function myFunction4(){
+
+}
+document.getElementById("hi").style.color="skyblue";
